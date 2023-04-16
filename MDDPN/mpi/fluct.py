bfefebecb4a5e932404a17968a38304d0743c361
Architect0rr <rickbatra0z@gmail.com>
2023-04-16 17:05:30 +0300

    Smth

--- MDDPN/mpi/fluct.py
+++ MDDPN/mpi/fluct.py
@@ -6,12 +6,13 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-# Last modified: 15-04-2023 23:06:56
+# Last modified: 16-04-2023 16:51:38
 
 # TODO:
 # Change parser description
 # Add description af parameter dT
 
+import re
 import csv
 import json
 import argparse
@@ -21,7 +22,7 @@ from pathlib import Path
 import numpy as np
 from numpy import typing as npt
 
-import adios2
+from . import adios2
 from .. import uw_constants as ucs
 
 
@@ -29,46 +30,48 @@ def rms(arr: npt.NDArray[np.uint32]) -> npt.NDArray[np.float32]:
     N = len(arr)
     average = np.sum(arr, axis=0) / N
     barr = np.sum((average - arr[:])**2, axis=0)
+    print(barr)
     return np.sqrt(barr / N)
 
 
-def process(file: Path, xi: float, time_step: float, wevery: int, dT: float):
-    hms = round(dT / xi / time_step / wevery)
-    print(f"Hms is {hms} steps")
-
-    with adios2.open(file.as_posix(), 'w') as adout:  # type: ignore
+def process(file: Path, hms: int, lc: int):
+    with adios2.open(file.as_posix(), 'r') as adout:  # type: ignore
         for step in adout:
             dist: npt.NDArray[np.uint32] = step.read("dist")
             break
 
     buffer: npt.NDArray[np.uint32] = np.zeros((hms, len(dist)), dtype=np.uint32)  # type: ignore
 
-    with adios2.open(file.as_posix(), 'w') as adout:  # type: ignore
+    with adios2.open(file.as_posix(), 'r') as adout:  # type: ignore
         total_steps = adout.steps()
         write_file = Path(file.as_posix() + ".csv")
         write_file.touch()
         with open(write_file, "w") as csv_file:
             writer = csv.writer(csv_file, delimiter=',')
-            counter = 0
+            counter = lc
             for step in adout:
                 dist: npt.NDArray[np.uint32] = step.read("dist")
 
                 buffer[counter, :] = dist
 
+                counter += 1
+
                 if counter == hms:
                     writer.writerow(rms(buffer))
                     counter = 0
                     csv_file.flush()
+                    print("written")
 
                 if step.current_step() == total_steps - 1:
                     break
+    return counter
 
 
-if __name__ == "___main__":
+def main():
     parser = argparse.ArgumentParser(prog="fluct.py", description='CHANGEME.')
     parser.add_argument('--debug', action='store_true', help='Debug, prints only parsed arguments')
     parser.add_argument('--dT', type=float, default=0.08, help='???')
-    parser.add_argument('storages', type=str, help='Folder in which search for ADIOS2 distribution matrices')
+    parser.add_argument('--re', type=str, help='File regex')
 
     args = parser.parse_args()
     if args.debug:
@@ -79,15 +82,32 @@ if __name__ == "___main__":
     with open(cwd / ucs.data_file, 'r') as f:
         fp = json.load(f)
 
-    storages: List[str] = json.loads(args.storages)
+    folder: Path = cwd / fp['data_processing_folder']
+
+    storages: List[str] = []
+    stf: Path
+    for stf in folder.iterdir():
+        if re.match(args.re, stf.relative_to(folder).as_posix()):
+            storages.append(stf.relative_to(folder).as_posix())
 
     for file in storages:
-        if not (cwd / ucs.data_processing_folder / file).exists():
-            raise FileNotFoundError(f"File {(cwd / ucs.data_processing_folder / file).as_posix()} cannot be found")
+        stf = cwd / fp['data_processing_folder'] / file
+        if not stf.exists():
+            raise FileNotFoundError(f"File {stf.as_posix()} cannot be found")
 
     try:
-        _temp = (fp['xi'], fp['dt'], fp['every'])
+        _temp = (fp['xi'], fp['time_step'], fp['every'])
     except KeyError:
         raise
+
+    hms = round(args.dT / fp['xi'] / fp["time_step"] / fp["every"])
+    print(f"Hms is {hms} steps")
+
+    lc = 0
     for file in storages:
-        process(cwd / ucs.data_processing_folder / file, fp['xi'], fp['dt'], fp['every'], args.dT)
+        stf = cwd / fp['data_processing_folder'] / file
+        lc = process(stf, hms, lc)
+
+
+if __name__ == "___main__":
+    main()
---
+++ fluct.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.8
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2023 Perevoshchikov Egor
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+# Last modified: 16-04-2023 16:08:53
+
+
+import sys
+
+from MDDPN.mpi import fluct
+
+
+sys.exit(fluct.main())
